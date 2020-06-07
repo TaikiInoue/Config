1623db528426941a71ab6de3b033ae396b4363a1
TaikiInoue <taikiinoue45@gmail.com>
2020-06-07 18:39:31 +0900

    Initial commit

--- install_cuda.sh
+++ install_cuda.sh
@@ -1,9 +1,10 @@
 # https://github.com/NVIDIA/nvidia-docker/wiki/Frequently-Asked-Questions#how-do-i-install-the-nvidia-driver
 # https://docs.nvidia.com/cuda/cuda-installation-guide-linux/index.html#package-manager-installation
+# https://medium.com/@exesse/cuda-10-1-installation-on-ubuntu-18-04-lts-d04f89287130
 
 # Install gcc for building cuda toolkit
 sudo apt update
-sudo apt install -y gcc
+sudo apt install -y build-essential
 
 # Verify the System has the Correct Kernel Headers
 sudo apt-get update
@@ -15,9 +16,9 @@ sudo mv cuda-ubuntu1804.pin /etc/apt/preferences.d/cuda-repository-pin-600
 sudo apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/7fa2af80.pub
 sudo add-apt-repository "deb http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/ /"
 sudo apt-get update
-sudo apt-get install -y cuda
+sudo apt-get install -y cuda-10-1
 
 # Environment setup
-echo 'export PATH=/usr/local/cuda-11.0/bin${PATH:+:${PATH}}' >> ~/.bashrc
-echo 'export LD_LIBRARY_PATH=/usr/local/cuda-11.0/lib64${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}' >> ~/.bashrc
+echo 'export PATH=/usr/local/cuda-10.1/bin${PATH:+:${PATH}}' >> ~/.bashrc
+echo 'export LD_LIBRARY_PATH=/usr/local/cuda-10.1/lib64${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}' >> ~/.bashrc
 source ~/.bashrc
