ef364dc2ee8a152ea5ea749c7c9c722d0d898fa5
TaikiInoue <taikiinoue45@gmail.com>
2020-06-27 19:24:22 +0900

    Rename

--- cuda-10-2.sh
+++ cuda-10-2.sh
@@ -1,6 +1,5 @@
 # https://github.com/NVIDIA/nvidia-docker/wiki/Frequently-Asked-Questions#how-do-i-install-the-nvidia-driver
 # https://docs.nvidia.com/cuda/cuda-installation-guide-linux/index.html#package-manager-installation
-# https://medium.com/@exesse/cuda-10-1-installation-on-ubuntu-18-04-lts-d04f89287130
 
 # Install gcc for building cuda toolkit
 sudo apt update
@@ -10,12 +9,17 @@ sudo apt install -y build-essential
 sudo apt-get update
 sudo apt-get install -y linux-headers-$(uname -r)
 
-# Install cuda
-# https://developer.nvidia.com/cuda-10.2-download-archive-base
-wget http://developer.download.nvidia.com/compute/cuda/10.2/Prod/local_installers/cuda_10.2.89_440.33.01_linux.run
-sudo sh cuda_10.2.89_440.33.01_linux.run
-rm ./cuda_10.2.89_440.33.01_linux.run
+# Install cuda-10-2
+# https://developer.nvidia.com/cuda-10.2-download-archive
+wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/cuda-ubuntu1804.pin
+sudo mv cuda-ubuntu1804.pin /etc/apt/preferences.d/cuda-repository-pin-600
+wget http://developer.download.nvidia.com/compute/cuda/10.2/Prod/local_installers/cuda-repo-ubuntu1804-10-2-local-10.2.89-440.33.01_1.0-1_amd64.deb
+sudo dpkg -i cuda-repo-ubuntu1804-10-2-local-10.2.89-440.33.01_1.0-1_amd64.deb
+sudo apt-key add /var/cuda-repo-10-2-local-10.2.89-440.33.01/7fa2af80.pub
+sudo apt-get update
+sudo apt-get -y install cuda-10-2
 
 # Environment setup
-echo PATH=/usr/local/cuda-10.2/bin${PATH:+:${PATH}} >> ~/.bachrc
-echo LD_LIBRARY_PATH=/usr/local/cuda-10.2/lib64${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}} >> ~/.bachrc
+echo export PATH=/usr/local/cuda-10.2/bin${PATH:+:${PATH}} >> ~/.bashrc
+echo export LD_LIBRARY_PATH=/usr/local/cuda-10.2/lib64${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}} >> ~/.bashrc
+sudo reboot
